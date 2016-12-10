df9db6f596d3401707888729204fd23697c1a503
Jon Elliott <jonnox@gmail.com>
2016-12-10 00:57:56 -0500

    answers

--- app.js
+++ app.js
@@ -1,9 +1,64 @@
 var fs = require('fs');
 
 var length = 0;
+
+var bots = {};
+
 function parseCommand(line) {
     var l = line;
-    
+
+    var result = (/value (\d+) goes to (bot \d+)/g).exec(l);
+
+    if (result) {
+        getValue(result[2], parseInt(result[1]));
+    } else {
+        result = (/(bot \d+) gives low to (\w+ \d+) and high to (\w+ \d+)/g).exec(l);
+        giveValue(result[1], result[3], result[2]);
+    }
+}
+
+function giveValue(bot, highDest, lowDest) {
+
+    if (! bots[bot]) {
+        bots[bot] = { high: null, low : null };
+    }
+
+    bots[bot].give = function() {
+        var high = bots[bot].high;
+        var low = bots[bot].low;
+        bots[bot] = { high: null, low : null };
+
+        getValue(highDest, high);
+        getValue(lowDest, low);
+    };
+
+    give(bot);
+}
+
+function getValue(bot, value) {
+    if (! bots[bot]) {
+        bots[bot] = { high: null, low : null };
+    }
+
+    if (value > bots[bot].high) {
+        bots[bot].low = bots[bot].high;
+        bots[bot].high = value;
+    } else {
+        bots[bot].low = value;
+    }
+
+    give(bot);
+}
+
+function give(bot) {
+    if (bots[bot].high != null && bots[bot].low != null && bots[bot].give) {
+
+        if (bots[bot].high == 61 && bots[bot].low == 17) {
+            console.log("Part 1: " + bot);
+        }
+
+        bots[bot].give();
+    }
 }
 
 fs.readFile('input.txt', 'utf8', function (err,data) {
@@ -16,5 +71,7 @@ fs.readFile('input.txt', 'utf8', function (err,data) {
   lines.forEach(function(e,i) {
       parseCommand(e.trim());
   });
+
+  console.log('Part 2: ' + (bots['output 0'].high * bots['output 1'].high * bots['output 2'].high));
 
 });
